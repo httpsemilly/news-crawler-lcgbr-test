e1bf8710e527e04f910afbc9e72d4d9497309946
Emilly Cavalcante <emilly.menezescs@gmail.com>
2026-03-07 12:43:09 -0300

    feat: define item schema for The Guardian articles

--- crawler/the_guardian/items.py
+++ crawler/the_guardian/items.py
@@ -1,12 +1,25 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
-
 class TheGuardianItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    """
+    Class that represents a news article scraped from The Guardian website.
+    
+    Attributes:
+        article_url: URL of the article.
+        headline: Main title of the article.
+        author: List of authors.
+        article_text: Full body text of the article.
+        published_date: Original publication date and time.
+        category: The Guardian section (e.g. World, Politics).
+        standfirst: Short summary displayed below the headline.
+        scraped_at: Timestamp of when the article was collected.
+    """
+
+    article_url = scrapy.Field()
+    headline = scrapy.Field()
+    author = scrapy.Field()
+    article_text = scrapy.Field()
+    published_date = scrapy.Field()
+    category = scrapy.Field()
+    standfirst = scrapy.Field()
+    scraped_at = scrapy.Field()
