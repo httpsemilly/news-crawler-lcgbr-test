2c2ed869fd4a915bf1aa8b6c59d08eac0b842722
Emilly Cavalcante <emilly.menezescs@gmail.com>
2026-03-07 23:07:18 -0300

    feat: implement BigQuery pipeline for article storage

--- crawler/the_guardian/pipelines.py
+++ crawler/the_guardian/pipelines.py
@@ -1,13 +1,50 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+from google.cloud import bigquery
+import yaml
 
 class TheGuardianPipeline:
-    def process_item(self, item, spider):
+    """
+    Pipeline for storing scraped articles into BigQuery.
+
+    Connects to BigQuery on spider_open and inserts each scraped article into the configured dataset and table.
+
+    Methods:
+        open_spider(): Initializes the BigQuery client and table reference when the spider opens.
+        process_item(): Inserts a scraped article item into BigQuery.
+    """
+
+    def open_spider(self):
+        """Initializes the BigQuery client and table reference when the spider opens."""
+
+        with open('config/config.yaml', 'r') as file:
+            config = yaml.safe_load(file)
+
+        self.dataset = config['bigquery']['dataset']
+        self.table = config['bigquery']['table']
+
+        self.client = bigquery.Client()
+
+        self.dataset_ref = self.client.dataset(self.dataset)
+        self.table_ref = self.dataset_ref.table(self.table)
+
+    def process_item(self, item):
+        """
+        Inserts a scraped article item into BigQuery.
+
+        Args:
+            item: A TheGuardianItem instance containing the article data.
+
+        Returns:
+            item: The item after successful insertion.
+
+        Raises:
+            Exception: If BigQuery returns errors during row insertion.
+        """
+
+        item = ItemAdapter(item).asdict()
+        errors = self.client.insert_rows_json(self.table_ref, [item])
+
+        if errors:
+            raise Exception(f"Encountered errors while inserting rows: {errors}")
+        
         return item
